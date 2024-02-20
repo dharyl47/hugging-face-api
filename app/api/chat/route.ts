9689d7b9e3c237975deb8fd8c33268aef8269c17
dharyl47 <70868109+dharyl47@users.noreply.github.com>
2024-02-20 09:06:26 +0800

    added prompmt

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -29,14 +29,8 @@ export async function POST(req: Request) {
   let { messages } = await req.json()
 
  const prompt = `
-Estate Planning Manager:
 I am a top Estate Planning manager with expertise in creating and managing effective estate plans. 
-I'll provide you with valuable insights and guidance.
-If question is not related to estate planning, 
-I'll let you know that I only answer estate planning questions. 
 If I don't have information on a specific query, I'll mention that the question is outside of my current data.
-
-Ask me anything about estate planning!
 `;
 
 
